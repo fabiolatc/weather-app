b497cb34673284b845dfaaebcb0c5a714ab8a1d2
fabiolatc <61229208+fabiolatc@users.noreply.github.com>
2021-06-23 00:54:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,55 +1,39 @@
-import "./App.css";
+import "./styles.css";
 import React, { useState } from "react";
 import axios from "axios";
-
 export default function App() {
   let [city, cityAnswer] = useState("");
   let [text, changeText] = useState("");
-
-  let [description, changeDescription] = useState("");
   let [descriptiontext, changeDescriptiontext] = useState("");
-
-  let [temperature, changeTemperature] = useState("");
   let [temperaturetext, changeTemperaturetext] = useState("");
-
-  let [humidity, changeHumidity] = useState("");
   let [humiditytext, changeHumiditytext] = useState("");
-
-  let [wind, changeWind] = useState("");
   let [windtext, changeWindtext] = useState("");
-
-  let [icon, changeIcon] = useState("");
   let [icontext, changeIcontext] = useState("");
-
   function updateCity(event) {
     cityAnswer(event.target.value);
   }
-
-  //
-
   function searchCity(event) {
     let APIkey = "3c7e72471b038017abb118fddfa1d953";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`;
     axios.get(url).then(cityTemperature);
     event.preventDefault();
     function cityTemperature(response) {
-      changeTemperature(Math.round(response.data.main.temp));
-      changeDescription(response.data.weather[0].description);
-      changeHumidity(response.data.main.humidity);
-      changeWind(response.data.wind.speed);
-      changeIcon(response.data.weather[0].icon);
+      changeText(`${city}`);
+      changeTemperaturetext(
+        `Temperature: ${Math.round(response.data.main.temp)}°C`
+      );
+      changeDescriptiontext(
+        `Description: ${response.data.weather[0].description}`
+      );
+      changeHumiditytext(`Humidity: ${response.data.main.humidity}%`);
+      changeWindtext(`Wind: ${response.data.wind.speed} km/h`);
+      changeIcontext(
+        <img
+          src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+        />
+      );
     }
-
-    changeText(`${city}`);
-    changeDescriptiontext(`Description: ${description}`);
-    changeTemperaturetext(`Temperature: ${temperature}°C`);
-    changeHumiditytext(`Humidity: ${humidity}%`);
-    changeWindtext(`Wind: ${wind} km/h`);
-    changeIcontext(
-      <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
-    );
   }
-
   return (
     <div className="App">
       <h1>Weather App</h1>
